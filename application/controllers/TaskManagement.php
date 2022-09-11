87e43b72eac24d9ab1252d874dc82bb15caeb665
Sundar Raj <sundarraj@usistech.com>
2022-09-11 15:25:44 +0530

    updated

--- application/controllers/TaskManagement.php
+++ application/controllers/TaskManagement.php
@@ -277,7 +277,7 @@
 
 				$data = array(
 
-					'task_details' => $this->TaskManagement_Model->searchWithKeyword($keyword),
+					'task_details' => $this->TaskManagement_Model->searchWithKeyword($keyword, $this->session->userdata('user_role')),
 					'employee_details' => $this->TaskManagement_Model->getEmployeeDetails()
 
 				);
@@ -301,7 +301,7 @@
 
 				$data = array(
 
-					'task_details' => $this->TaskManagement_Model->searchWithDueDate($due_date),
+					'task_details' => $this->TaskManagement_Model->searchWithDueDate($due_date, $this->session->userdata('user_role'), $this->session->userdata('user_name')),
 					'employee_details' => $this->TaskManagement_Model->getEmployeeDetails()
 
 				);
